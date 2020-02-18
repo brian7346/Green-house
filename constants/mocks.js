585d8d45bfa47c82bedcb7c1d7ac197d46da5fd8
user <user@users-Mac-mini.local>
2020-02-18 12:27:28 +0400

    feat: pruducts mock, images

--- constants/mocks.js
+++ constants/mocks.js
@@ -43,10 +43,33 @@ const categories = [
   },
 ];
 
-const products = [];
+const products = [
+  {
+    id: 1,
+    name: '16 Best Plants That Thrive In Your Bedroom',
+    description:
+      'Bedrooms deserve to be decorated with lush greenery just like every other room in the house – but it can be tricky to find a plant that thrives here. Low light, high humidity and warm temperatures mean only certain houseplants will flourish.',
+    tags: ['Interior', '27m', 'Ideas'],
+    gallery: [
+      require('../assets/images/plants_1.png'),
+      require('../assets/images/plants_2.png'),
+      require('../assets/images/plants_3.png'),
+      require('../assets/images/plants_1.png'),
+      require('../assets/images/plants_2.png'),
+      require('../assets/images/plants_3.png'),
+    ],
+  },
+];
 
-const explore = [];
+const explore = [
+  require('../assets/images/explore_1.png'),
+  require('../assets/images/explore_2.png'),
+  require('../assets/images/explore_3.png'),
+  require('../assets/images/explore_4.png'),
+  require('../assets/images/explore_5.png'),
+  require('../assets/images/explore_6.png'),
+];
 
 const profile = [];
 
-export { categories, products, profile };
+export { categories, products, explore, profile };

81d51458607d2d5c9b2d4474a73edc9cc7539a0d
user <user@users-Mac-mini.local>
2020-02-18 12:18:09 +0400

    feat: categories mock, images

--- constants/mocks.js
+++ constants/mocks.js
@@ -1,6 +1,51 @@
-const categories = [];
+const categories = [
+  {
+    id: 'plants',
+    name: 'Plants',
+    tags: ['products', 'inspirations'],
+    count: 15,
+    image: require('../assets/images/illustration_1.png'),
+  },
+  {
+    id: 'seeds',
+    name: 'Seeds',
+    tags: ['products', 'shop'],
+    count: 9,
+    image: require('../assets/images/llustration_2.png'),
+  },
+  {
+    id: 'flowers',
+    name: 'Flowers',
+    tags: ['products', 'inspirations'],
+    count: 18,
+    image: require('../assets/images/llustration_3.png'),
+  },
+  {
+    id: 'sprayers',
+    name: 'Sprayers',
+    tags: ['products', 'shop'],
+    count: 17,
+    image: require('../assets/images/llustration_1.png'),
+  },
+  {
+    id: 'pots',
+    name: 'Pots',
+    tags: ['products', 'shop'],
+    count: 23,
+    image: require('../assets/images/llustration_2.png'),
+  },
+  {
+    id: 'fertilizers',
+    name: 'Fertilizers',
+    tags: ['products', 'inspirations'],
+    count: 12,
+    image: require('../assets/images/llustration_3.png'),
+  },
+];
 
 const products = [];
+
+const explore = [];
 
 const profile = [];
 

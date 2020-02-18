e120a7a8604205381e4b9c9d1b2e360e704582a1
user <user@users-Mac-mini.local>
2020-02-18 11:39:18 +0400

    initial commit

---
+++ App.js
@@ -0,0 +1,28 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React from 'react';
+import {StyleSheet, View, Text} from 'react-native';
+
+const App: () => React$Node = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Hello World!</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
+
+export default App;

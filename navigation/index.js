6dec4e3191dfdaec8e31a20af71dfbf7c02121cc
user <user@users-Mac-mini.simbirsoft>
2020-02-19 17:42:02 +0400

    feat: login navigation bar

--- navigation/index.js
+++ navigation/index.js
@@ -1,12 +1,39 @@
+import * as React from 'react';
+import { Image } from 'react-native';
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 
 import Welcome from '../screens/Welcome';
 import Login from '../screens/Login';
+import { theme } from '../constants';
 
-const screens = createStackNavigator({
-  Welcome,
-  Login,
-});
+const screens = createStackNavigator(
+  {
+    Welcome,
+    Login,
+  },
+  {
+    defaultNavigationOptions: {
+      headerTitle: null,
+      headerBackTitleVisible: null,
+      headerStyle: {
+        height: theme.sizes.base * 6,
+        backgroundColor: theme.colors.white,
+        shadowColor: 'transparent',
+        elevalion: 0,
+      },
+      headerBackImage: <Image source={require('../assets/icons/back.png')} />,
+      headerLeftContainerStyle: {
+        alignItems: 'center',
+        marginLeft: theme.sizes.base * 2,
+        paddingRight: theme.sizes.base,
+      },
+      headerRightContainerStyle: {
+        alignItems: 'center',
+        paddingRight: theme.sizes.base,
+      },
+    },
+  },
+);
 
 export default createAppContainer(screens);

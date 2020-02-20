43592552b81e5fd8b4e781bbe310472ecd54cf07
user <user@users-Mac-mini.local>
2020-02-20 14:52:41 +0400

    feat: icons

--- components/index.js
+++ components/index.js
@@ -1,5 +1,6 @@
 import Block from './Block';
 import Text from './Text';
 import Button from './Button';
+import Input from './Input';
 
-export { Block, Text, Button };
+export { Block, Text, Button, Input };
--- screens/Login.js
+++ screens/Login.js
@@ -1,16 +1,38 @@
 import * as React from 'react';
-import { View } from 'react-native';
-import { Block, Text, Button } from '../components';
+import { StyleSheet } from 'react-native';
+import { Block, Text, Button, Input } from '../components';
 import { theme } from '../constants';
 
 const Login = () => {
+  const [email, setEmail] = React.useState('');
+  const [password, setPassword] = React.useState('');
+
   return (
     <Block padding={[0, theme.sizes.base * 2]} flex color="white">
       <Text h1 bold>
         Login
       </Text>
+      <Block middle>
+        <Input
+          label="Email"
+          style={styles.input}
+          defaultValue={email}
+          onChangeText={text => setEmail(text)}
+        />
+        <Input
+          secure
+          label="Password"
+          style={styles.input}
+          defaultValue={password}
+          onChangeText={pass => setPassword(pass)}
+        />
+      </Block>
     </Block>
   );
 };
+
+const styles = StyleSheet.create({
+  input: {},
+});
 
 export default Login;

43bb7acd4957866ed0677ae6f76c7fe953624633
brian7346 <brian2009@yandex.ru>
2020-02-22 21:26:15 +0400

    feat: finish ligin screen

--- screens/Login.js
+++ screens/Login.js
@@ -1,48 +1,75 @@
 import * as React from 'react';
-import { StyleSheet, KeyboardAvoidingView } from 'react-native';
+import { StyleSheet, ActivityIndicator } from 'react-native';
 import { Block, Text, Input, Button } from '../components';
 import { theme } from '../constants';
 
-const Login = () => {
+const Login = props => {
   const [email, setEmail] = React.useState('doctorbrian97@gmail.com');
   const [password, setPassword] = React.useState('11111111');
+  const [loading, setLoading] = React.useState(false);
+  const [error, setError] = React.useState(false);
+  const { navigation } = props;
 
   const setLogin = () => {
-    return null;
+    setLoading(true);
+
+    if (email === '' || password === '') {
+      setTimeout(() => {
+        setError(true);
+        setLoading(false);
+      }, 500);
+    }
+
+    if (!error) {
+      setTimeout(() => {
+        setLoading(false);
+        navigation.navigate('Browse');
+      }, 500);
+    }
+  };
+
+  const hasError = () => {
+    if (error) {
+      return styles.hasError;
+    }
   };
 
   return (
-      <Block padding={[0, theme.sizes.base * 2]} color="white">
-        <Text h1 bold>
-          Login
-        </Text>
-        <Block middle>
-          <Input
-            label="Email"
-            style={styles.input}
-            defaultValue={email}
-            onChangeText={text => setEmail(text)}
-          />
-          <Input
-            secure
-            label="Password"
-            style={styles.input}
-            defaultValue={password}
-            onChangeText={pass => setPassword(pass)}
-          />
-          <Button color="primary" gradient onPress={() => setLogin()}>
+    <Block padding={[0, theme.sizes.base * 2]} color="white">
+      <Text h1 bold>
+        Login
+      </Text>
+      <Block middle>
+        <Input
+          label="Email"
+          style={[styles.input, hasError()]}
+          defaultValue={email}
+          onChangeText={text => setEmail(text)}
+        />
+        <Input
+          secure
+          label="Password"
+          style={[styles.input, hasError()]}
+          defaultValue={password}
+          onChangeText={pass => setPassword(pass)}
+        />
+        <Button color="primary" gradient onPress={() => setLogin()}>
+          {loading ? (
+            <ActivityIndicator color="white" />
+          ) : (
             <Text bold white center>
               Login
             </Text>
-          </Button>
+          )}
+        </Button>
 
-          <Button onPress={() => null}>
-            <Text style={styles.forgotText} gray caption center>
-              Forgot your password?
-            </Text>
-          </Button>
-        </Block>
+        <Button onPress={() => navigation.navigate('Forgot')}>
+          <Text style={styles.forgotText} gray caption center>
+            Forgot your password?
+          </Text>
+        </Button>
       </Block>
+    </Block>
   );
 };
 
@@ -59,6 +86,9 @@ const styles = StyleSheet.create({
   },
   forgotText: {
     textDecorationLine: 'underline',
+  },
+  hasError: {
+    borderBottomColor: theme.colors.accent,
   },
 });
 
